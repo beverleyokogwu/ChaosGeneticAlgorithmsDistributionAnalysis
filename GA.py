53657684e74dc5e6b69b927457f1963203bc12ee
beverleyokogwu <beverley.okogwu@gmail.com>
2020-11-16 15:27:45 -0500

    added histograms of shift-scale distributions

--- GA.py
+++ GA.py
@@ -9,8 +9,23 @@ from numpy.random import randn
 
 genarrayav=[]
 genarraymin=[]
+mutd_values =[] #store the mutated values
+
+#Histogram plotting function
+def plotHistogram(map_array, name):
 
 
+    n, bins, patches = plt.hist(x=map_array, bins=20, color='#0504aa',
+                            alpha=0.7, rwidth=0.85)
+    plt.grid(axis='y', alpha=0.75)
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.title(name)
+
+    maxfreq = n.max()
+    # Set a clean upper y-axis limit.
+    plt.ylim(ymax=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 10)
+    plt.show()
 
 # Logistic Class
 class LM:
@@ -160,6 +175,9 @@ def mutate(individual,probability,algorithm_object):
         num = random.uniform(0.0,1.0)
         if num < probability:
             n_val=algorithm_object.shift_scale_next()
+            print("\nAdding {} to the mutated array....".format(n_val))
+            mutd_values.append(n_val)
+            print("mutd_array now contains {} values with the first value ->{} and the last value-> {}".format(len(mutd_values), mutd_values[0], mutd_values[-1]))
             individual[gene]+=n_val
 
     return individual
@@ -277,6 +295,7 @@ def plots():
     mins2D_GA=[]
 
 
+    #DEALS WITH CGA
     for i in range(num_trails):
 
         #reset
@@ -310,7 +329,13 @@ def plots():
         print("mins2D_CGA now contains...")
         print(mins2D_CGA)
 
+    CGA_mutd_values = mutd_values
+    print("\n\n\nTHE SHIFT SCALE VALUES ARE:\nCGA:")
+    print(CGA_mutd_values)
+    print("clearing the mutd array......................................")
+    mutd_values.clear()
 
+    #DEALS WITH GA
     for j in range(num_trails):
 
         #reset
@@ -348,7 +373,7 @@ def plots():
         print("mins2D_GA now contains...")
         print(mins2D_GA)
 
-
+    GA_mutd_values = mutd_values
 
     #HERE! - PRINT THE ARRAYS (WITH A SMALL SET) & COMPARE THEM
     print("\nTHE 2D ARRAYS FOR GA AND CGA: \nCGA:\nAVERAGES-")
@@ -361,6 +386,10 @@ def plots():
     print("MINS-")
     print(mins2D_GA)
 
+    print("\n\n\nTHE SHIFT SCALE VALUES ARE:\nCGA:")
+    print(CGA_mutd_values)
+    print("GA:")
+    print(GA_mutd_values)
 
     #at the end, should have two 2D arrays
     #use the np mean and std dev to compute them in an array
@@ -396,7 +425,7 @@ def plots():
     plt.errorbar(x, avg_mins_GA, std_mins_GA,  label = "average-fitness of mins (GA)")
     """
 
-    #SUBPLOTS
+    #SUBPLOTS FOR ANG AND MIN EAs
     fig,ax = plt.subplots(2,2)
     ax[0,0].plot(x, avg_avgs_GA)
     ax[0,0].set_title('Average of Average Fitness per Genaration (GA)')
@@ -411,6 +440,33 @@ def plots():
         ax.set(xlabel='generation', ylabel='fitness')
         ax.label_outer()
 
+
+    plt.show()
+
+    #SUBPLOTS FOR DISTRIBUTIONS
+
+    plotHistogram(CGA_mutd_values,'Shift-Scale Distributions for CGA')
+
+    plotHistogram(GA_mutd_values,'Shift-Scale Distributions for GA')
+
+    figs,axs = plt.subplots(2,1)
+    n, bins, patches = axs[0,0].hist(x=CGA_mutd_values, bins=20, color='#0504aa',alpha=0.7, rwidth=0.85)
+    axs[0,0].set_title('Shift-Scale Distributions for CGA')
+    axs[0,0].grid(axis='y', alpha=0.75)
+    maxfreq = n.max()
+    # Set a clean upper y-axis limit.
+    axs[0,0].ylim(ymax=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 10)
+
+    n, bins, patches = axs[1,0].hist(x=GA_mutd_values, bins=20, color='#0504aa',alpha=0.7, rwidth=0.85)
+    axs[1,0].grid(axis='y', alpha=0.75)
+    axs[1,0].set_title('Shift-Scale Distributions for GA')
+    maxfreq = n.max()
+    # Set a clean upper y-axis limit.
+    axs[1,0].ylim(ymax=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 10)
+
+    for ax in axs.flat:
+        ax.set(xlabel='values', ylabel='frequency')
+        ax.label_outer()
 
     plt.show()
 
